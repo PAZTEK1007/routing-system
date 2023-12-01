90d805c0b9e8030bd2aa9f965e89af8ae59530d4
Mathias Barbier <mathbarbier07@gmail.com>
2023-12-01 09:49:37 +0100

    update

--- src/index.php
+++ src/index.php
@@ -9,15 +9,15 @@ use App\Http\Request;
 use App\Controller\HomepageController;
 use App\Controller\ArticleController;
 
-$request = new Request($_GET['url'] ?? '/');
+
+$request = new Request();
 $router = new Router($request);
 
-// Inclure vos routes depuis routes.php
 require 'Router/routes.php';
 
 try {
     $router->resolve();
 } catch (Exception $e) {
-    // Gérer les erreurs de routage ici, par exemple, rediriger vers une page d'erreur 404
+
     require 'View/404.php';
 }
