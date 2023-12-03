923297cd92e002861e31d426fd1e03c07f64d0e2
Mathias Barbier <mathbarbier07@gmail.com>
2023-12-03 20:47:34 +0100

    push files

--- src/index.php
+++ src/index.php
@@ -1,71 +1,24 @@
 <?php
-declare(strict_types=1);
+require '../vendor/autoload.php';
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-require_once '../vendor/autoload.php';
-// Include all your model files here
-use App\Model\Article;
-use App\Model\Author;
+use App\Router\Router;
 
 
-// Include all your controllers here
+$router = new Router();
 
-use App\Controller\ArticleController;
-use App\Controller\AuthorController;
-use App\Controller\HomepageController;
+$router->addRoutes('GET', '/', function() {
+    echo 'Hello World';
+});
 
+$router->addRoutes('GET', '/pizza', function() {
+    echo 'Hello Pizza';
+});
 
-// Start the session once
-session_start();
+$router->addRoutes('GET', '/hello/:name', function($name) {
+    echo 'Hello ' . $name;
+});
 
-// Get the current page to load
-// If nothing is specified, it will remain empty (home should be loaded)
-$page = $_GET['page'] ?? null;
-
-// Load the controller
-// It will *control* the rest of the work to load the page
-switch ($page) 
-{
-    case 'articles':
-        $articleController = new ArticleController();
-        $articleController->index();
-        break;
-   
-    case 'articles-show':
-        $articleController = new ArticleController();
-        $articleController->show();
-        break;
-
-    case 'articles-create':
-        $articleController = new ArticleController();
-        $articleController->create();
-        break;
-        
-    case 'articles-update':
-        $articleController = new ArticleController();
-        $articleController->update();
-        break;
-
-    case 'articles-delete':
-        $articleController = new ArticleController();
-        $articleController->delete();
-        break;
-
-    case 'authors':
-        $authorController = new AuthorController();
-        $authorController->index();
-        break;
-
-    case 'authors-show':
-        $authorController = new AuthorController();
-        $authorController->show();
-        break;
-
-    case 'home':
-        $homepageController = new HomepageController();
-        $homepageController->index();
-        break;
-}
+$router->matchRoute();
+echo '<pre>';
+var_dump($router);
+echo '</pre>';
